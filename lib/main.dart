e5fc576c597ea9fa3e7227e0ce7b6eb318ac4d0e
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-08 20:07:52 +0530

    Completed. Dart probably doesn't have nested interpolation functionality and also to use template and string interpolation together.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -15,29 +16,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace(String side) {
+    var side1 = '$side';
+//    print("Value of side1 = $side");
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+    var side2 = '$side1';
+    print("$side button pressed");
+    print(
+        '$side dice number = ${side2}DiceNumber'); //string interpolation inside string interpolation is not possible in Dart
+  }
+
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 5;
-
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
 //          flex: 1,
             child: FlatButton(
-              child: Image.asset("images/dice$leftDiceNumber.png"),
               onPressed: () {
-                print("Left button pressed");
+                changeDiceFace('Left');
               },
+              child: Image.asset("images/dice$leftDiceNumber.png"),
             ),
           ),
           Expanded(
 //          flex: 2,
             child: FlatButton(
-              child: Image.asset("images/dice1.png"),
+              child: Image.asset("images/dice$rightDiceNumber.png"),
               onPressed: () {
-                print("Right button pressed.");
+                changeDiceFace('Right');
               },
             ),
           ),
